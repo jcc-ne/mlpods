2ceb0dd31d3a4c5fa7c0a19d677a653c779230e5
janine c <janine.cheng@gmail.com>
2017-08-20 01:04:39 -0700

    pypi version change + setup

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'mlpods',
   packages = ['mlpods'], # this must be the same as the name above
-  version = '0.1',
+  version = '0.1.1',
   description = 'MLpods',
   author = 'Janine Cheng, Ketan Patel',
   author_email = 'janine.cheng@mlpods.com',
   url = 'https://github.com/jcc-ne/mlpods', # use the URL to the github repo
-  download_url = 'https://github.com/jcc-ne/mlpods/archive/0.1.tar.gz',
+  download_url = 'https://github.com/jcc-ne/mlpods/archive/0.1.1.tar.gz',
   keywords = ['MLpods', 'deployment', 'machine learning',
               'ML', 'serverless'], # arbitrary keywords
   classifiers = [],
