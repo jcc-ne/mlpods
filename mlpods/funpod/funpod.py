3c067cd47c86c30323425750fe7a8dc31a0ec5cf
janine c <janine.cheng@gmail.com>
2017-08-24 16:15:55 -0700

    v0.1.5 add multiports support

--- mlpods/funpod/funpod.py
+++ mlpods/funpod/funpod.py
@@ -1,8 +1,12 @@
 import socket
 import json
-#  import cPickle
+import cPickle
 import tempfile
 import docker
+import time
+
+
+MAX_TRY = 20
 
 
 class FunPod(object):
@@ -11,36 +15,64 @@ class FunPod(object):
         self.name = name
         self.docker_client = docker.from_env()
         self.container = None
-        self.fpc = FunPodConnector()
+        self._ip = '0.0.0.0'
+        self._port = 9998
+        self.connector = FunPodConnector(funpod=self)
+
+    @property
+    def port(self):
+        return self._port
+
+    @property
+    def ip(self):
+        return self._ip
+
+    @port.setter
+    def port(self, val):
+        self._port = val
+
+    @ip.setter
+    def ip(self, val):
+        self._ip = val
 
     def build(self, func):
         """TODO: build image given handle function """
         pass
 
-    def spinup(self):
-        self.container = self.docker_client.containers.run(
-                        self.name,
-                        detach=True,
-                        ports={'9998': '9998'})
-
-    def kill(self):
+    def find_siblings(self):
+        list_sib = [c for c in self.docker_client.containers.list()
+                    if c.attrs['Config']['Image'] == self.name]
         if self.container:
+            list_sib = [c for c in list_sib if c.id != self.container.id]
+        return list_sib
+
+    def spinup(self):
+        n = 0
+        success = False
+        while True:
             try:
-                print self.container.status
-                self.container.kill()
-            except Exception as e:
-                print e
-
-
-class FunPodConnector(object):
-
-    def __init__(self, ip='0.0.0.0', port=9998):
-        self.ip = ip
-        self.port = port
+                self.container = self.docker_client.containers.run(
+                    self.name,
+                    detach=True,
+                    ports={str(self._port): str(self.connector.port)})
+                success = True
+                break
+            except docker.errors.APIError:
+                # try a different port
+                self.connector.port -= 1
+                n += 1
+                if n > MAX_TRY:
+                    break
+        if not success:
+            raise Exception('Spinning up funpod {} not successful'.
+                            format(self.name))
+        print 'Funpod {} is spun up, port: {}'.format(
+            self.name, self.connector.port)
+        return self.connector.port
 
     def handle(self, func):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.bind((self.ip, self.port))
+        self.sock.bind((self._ip, self._port))
         self.sock.listen(1)
 
         conn, addr = self.sock.accept()
@@ -66,7 +98,7 @@ class FunPodConnector(object):
 
     def handle_file(self, func):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.bind((self.ip, self.port))
+        self.sock.bind((self._ip, self._port))
         self.sock.listen(1)
 
         conn, addr = self.sock.accept()
@@ -99,44 +131,114 @@ class FunPodConnector(object):
         f = self.recv_file(conn=conn)
         return f
 
+    def kill(self, c=None):
+        if not c:
+            c = self.container
+        if c:
+            try:
+                print 'killing', c.name, '... (current status:', c.status, ')'
+                c.kill()
+            except Exception as e:
+                print e
+
+    def kill_all(self, force=False):
+        if not force:
+            raise Exception('to execute kill_all, explicitly force by'
+                            'passing arg: > kill_all(force=True)')
+        self.kill(self.container)
+        for c in self.find_siblings():
+            self.kill(c)
+
+
+class FunPodConnector(object):
+
+    def __init__(self, name=None, funpod=None, ip='0.0.0.0', port=9998):
+        if name and funpod:
+            raise Exception('Args name and funpod cannot coexist')
+        elif not name and not funpod:
+            raise Exception('Please pass arg name or funpod')
+        self.funpod = funpod
+        if funpod:
+            self.name = funpod.name
+        else:
+            self.name = name
+        self._ip = ip
+        self._port = port
+
+    @property
+    def port(self):
+        return self._port
+
+    @property
+    def ip(self):
+        return self._ip
+
+    @port.setter
+    def port(self, val):
+        self._port = val
+
+    @ip.setter
+    def ip(self, val):
+        self._ip = val
+
     def send(self, data_send):
-        try:
-            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            self.sock.connect((self.ip, self.port))
-            #  self.sock.sendall(json.dumps(data_send).encode('utf-8'))
-            self.sock.sendall(data_send)
-            self.sock.shutdown(socket.SHUT_WR)
-            buf = ''
-            data = True
-            while data:
-                data = self.sock.recv(2048)
-                buf += data
-                #  return json.loads(unicode(buf, 'utf-8'))
-        finally:
-            self.sock.close()
+        n = 0
+        while True:
+            try:
+                self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                self.sock.connect((self._ip, self._port))
+                #  self.sock.sendall(json.dumps(data_send).encode('utf-8'))
+                self.sock.sendall(data_send)
+                self.sock.shutdown(socket.SHUT_WR)
+                buf = ''
+                data = True
+                while data:
+                    data = self.sock.recv(2048)
+                    buf += data
+                    #  return json.loads(unicode(buf, 'utf-8'))
+            except Exception as e:
+                n += 1
+                if n > MAX_TRY:
+                    raise Exception('Maxmum try reached')
+                time.sleep(0.5)
+                print 'caught', e, ', trying again...', n
+            else:
+                break
+            finally:
+                self.sock.close()
 
     def recv(self, conn=None):
-        try:
-            if not conn:
-                self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                self.sock.connect((self.ip, self.port))
-                #  self.sock.sendall(json.dumps(data_send).encode('utf-8'))
-                conn = self.sock
+        n = 0
+        while True:
+            try:
+                if not conn:
+                    self.sock = socket.socket(
+                        socket.AF_INET, socket.SOCK_STREAM)
+                    self.sock.connect((self._ip, self._port))
+                    #  self.sock.sendall(json.dumps(data_send).encode('utf-8'))
+                    conn = self.sock
 
-            buf = ''
-            data = True
-            while data:
-                data = conn.recv(2048)
-                buf += data
-            return buf
-        finally:
-            conn.close()
+                buf = ''
+                data = True
+                while data:
+                    data = conn.recv(2048)
+                    buf += data
+                return buf
+
+            except Exception as e:
+                n += 1
+                if n > MAX_TRY:
+                    raise Exception('Maxmum try reached')
+                print 'caught', e, ', trying again...', n
+
+            finally:
+                conn.close()
 
     def recv_file(self, conn=None):
         try:
             if not conn:
                 self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                self.sock.connect((self.ip, self.port))
+                self.sock.connect((self._ip, self._port))
                 #  self.sock.sendall(json.dumps(data_send).encode('utf-8'))
                 conn = self.sock
 
@@ -153,7 +255,7 @@ class FunPodConnector(object):
 
     def serve_generator(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.bind((self.ip, self.port))
+        self.sock.bind((self._ip, self._port))
         self.sock.listen(1)
 
         while True:
@@ -178,16 +280,53 @@ class FunPodConnector(object):
             finally:
                 conn.close()
 
-    def client_generator(self, fileobj=False, **kwargs):
-        if not fileobj:
-            self.send(json.dumps(kwargs))
+    def start_or_restart_pod(self):
+        if not self.funpod:
+            self.funpod = FunPod(self.name)
+        if self.funpod.container in self.funpod.docker_client.containers.list():
+            print "Funpod is already up, try restarting ..."
+            self.funpod.container.restart()
         else:
-            self.send(fileobj.read())
-        while True:
-            recv = self.recv()
-            if recv == 'EOF':
-                break
-            yield recv
+            self.funpod.spinup()
+        time.sleep(0.25)
+
+    def client_generator(self, serializer=None, fileobj=False, **kwargs):
+        """
+        client generator calls FunPod and return a generator
+        Args:
+        -----
+        serializer: (opt.) serializer that has dumps/loads functions.
+                    default cPickle
+        fileobj: file object. buffer will be transferred to function args
+        **kwargs: non-file type args to pass to function
+
+        Returns:
+        --------
+        result: generator. a python generator which items can be retrieved by
+                results.next() or all at once list(results)
+        """
+        if not serializer:
+            serializer = cPickle
+
+        if fileobj and kwargs:
+            raise Exception('client generator does not accept '
+                            'only either fileobj or kwargs')
+        try:
+            self.start_or_restart_pod()
+            if not fileobj:
+                self.send(json.dumps(kwargs))
+            else:
+                self.send(fileobj.read())
+            while True:
+                recv = self.recv()
+                if recv == 'EOF':
+                    break
+                #  yield serializer.loads(recv)
+                yield serializer.loads(recv)
+        except Exception as e:
+            print e
+        finally:
+            self.funpod.kill()
 
 
 def main_test():
--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'mlpods',
   packages = ['mlpods', 'mlpods.funpod'], # this must be the same as the name above
-  version = '0.1.3',
+  version = '0.1.5',
   description = 'MLpods',
   author = 'Janine Cheng, Ketan Patel',
   author_email = 'janine.cheng@mlpods.com',
   url = 'https://github.com/jcc-ne/mlpods', # use the URL to the github repo
-  download_url = 'https://github.com/jcc-ne/mlpods/archive/0.1.3.tar.gz',
+  download_url = 'https://github.com/jcc-ne/mlpods/archive/0.1.5.tar.gz',
   keywords = ['MLpods', 'deployment', 'machine learning',
               'ML', 'serverless'], # arbitrary keywords
   classifiers = [],
